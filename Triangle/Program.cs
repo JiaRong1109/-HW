9b49bf4930eb3fa3225b739fd08688224c9275b7
JiaRong <121345905+JiaRong1109@users.noreply.github.com>
2022-12-24 21:48:51 +0800

    Add files via upload

---
+++ Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿using DrawTriangle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Triangle.Triangle.Triangle_pic();
+            Console.WriteLine(DrawTriangle.DrawTriangle.Draw(5,DrawTriangle.DrawTriangle.Left));
+            Console.WriteLine(DrawTriangle.DrawTriangle.Draw(6, DrawTriangle.DrawTriangle.Right));
+            Console.WriteLine(DrawTriangle.DrawTriangle.Draw(8, DrawTriangle.DrawTriangle.Center));
+        }
+    }
+}
